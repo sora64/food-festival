e155bf442d2161b7c37281927eab80da56850c45
Alan Phelps <phelpsa64@gmail.com>
2022-04-08 14:06:53 -0400

    made application a PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const path = require("path");
 const webpack = require("webpack");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
 
 module.exports = {
   entry: {
@@ -11,7 +13,7 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: `${__dirname} /dist`,
+    path: `${__dirname}/dist`,
   },
   devServer: {
     static: "./",
@@ -47,6 +49,23 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
     }),
   ],
   mode: "development",
